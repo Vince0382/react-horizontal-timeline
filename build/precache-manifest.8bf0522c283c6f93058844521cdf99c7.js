2f2dbb30714995c68d2e10fa8fbb6928a3e8566a
PETRINI Vincent <vincent.petrini@gmail.com>
2019-12-01 09:39:31 +0100

    .

---
+++ build/precache-manifest.8bf0522c283c6f93058844521cdf99c7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44ae3b37fce9ae56f59f893fc8424d7e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f65241acd7abeae71dc2",
+    "url": "/static/css/main.b66e3c0a.chunk.css"
+  },
+  {
+    "revision": "5a7cbf37021ba0561144",
+    "url": "/static/js/2.7eb828f2.chunk.js"
+  },
+  {
+    "revision": "f65241acd7abeae71dc2",
+    "url": "/static/js/main.8753b179.chunk.js"
+  },
+  {
+    "revision": "72491971b672ba47af99",
+    "url": "/static/js/runtime-main.40129a76.js"
+  }
+]);
